40b777bd68276f914d74a01bf90933ff8bcfe0de
Mr_okushama <r-okushama@hotmail.com>
2014-04-30 15:51:13 +0100

    ported to 1.7 by @LusterBelle

--- okushama/drama/ProxyClient.java
+++ okushama/drama/ProxyClient.java
@@ -1,15 +1,14 @@
 package okushama.drama;
 
+import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
-import cpw.mods.fml.common.registry.TickRegistry;
-import cpw.mods.fml.relauncher.Side;
 
 public class ProxyClient extends ProxyCommon {
 
 	@Override
 	public void onModInit(FMLInitializationEvent evt) {
 		tickHandler = new TickHandlerClient();
-		TickRegistry.registerTickHandler(tickHandler, Side.CLIENT);
+		FMLCommonHandler.instance().bus().register(tickHandler);
 		super.onModInit(evt);
 	}
 
--- okushama/drama/TickHandlerClient.java
+++ okushama/drama/TickHandlerClient.java
@@ -7,11 +7,13 @@ import java.util.EnumSet;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiMainMenu;
-import cpw.mods.fml.common.ITickHandler;
 import cpw.mods.fml.common.ObfuscationReflectionHelper;
-import cpw.mods.fml.common.TickType;
+import cpw.mods.fml.common.eventhandler.Event;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.gameevent.TickEvent.RenderTickEvent;
+import cpw.mods.fml.relauncher.Side;
 
-public class TickHandlerClient implements ITickHandler {
+public class TickHandlerClient {
 
 	public boolean hasSet = false;
 
@@ -24,16 +26,16 @@ public class TickHandlerClient implements ITickHandler {
 		return getFirstLineOfSite("http://asie.pl/drama.php?count");
 	}
 
-	public String getFirstLineOfSite(String url){
+	public String getFirstLineOfSite(String url) {
 		try {
 			URL ourl = new URL(url);
-			BufferedReader in = new BufferedReader(new InputStreamReader(ourl.openStream()));
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					ourl.openStream()));
 			String txt;
 			while ((txt = in.readLine()) != null) {
 				return txt;
 			}
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
@@ -42,56 +44,50 @@ public class TickHandlerClient implements ITickHandler {
 
 	public int countCache = -1;
 
-	@Override
-	public void tickStart(EnumSet<TickType> type, Object... tickData) {
-		if (type.contains(TickType.CLIENT)) {
-			try {
-				Minecraft mc = Minecraft.getMinecraft();
-				if (mc.currentScreen != null) {
-					if (mc.currentScreen instanceof GuiMainMenu) {
-						GuiMainMenu mm = (GuiMainMenu)mc.currentScreen;
-						if (!hasSet) {
-							ObfuscationReflectionHelper.setPrivateValue(GuiMainMenu.class, mm, asieSplash(), "splashText" ,"field_73975_c", "r");
-							countCache = Integer.parseInt(asieCount());
-							hasSet = true;
-						}
-					}
-					else {
-						hasSet = false;
-					}
-				}
-			}
-			catch (Exception e) {
-				e.printStackTrace();
-			}
+	@SubscribeEvent
+	public void start(RenderTickEvent event) {
+		if (event.side.equals(Side.CLIENT)) {
+			tickStart();
+			tickEnd();
 		}
-
 	}
 
-	@Override
-	public void tickEnd(EnumSet<TickType> type, Object... tickData) {
+	public void tickStart() {
 		try {
 			Minecraft mc = Minecraft.getMinecraft();
 			if (mc.currentScreen != null) {
 				if (mc.currentScreen instanceof GuiMainMenu) {
-					GuiMainMenu mm = (GuiMainMenu)mc.currentScreen;
-					mm.drawString(mc.fontRenderer, countCache+" dramas generated and counting!", 2, 2, 0xffffff);
+					GuiMainMenu mm = (GuiMainMenu) mc.currentScreen;
+					if (!hasSet) {
+						ObfuscationReflectionHelper.setPrivateValue(
+								GuiMainMenu.class, mm, asieSplash(),
+								"splashText", "field_73975_c", "r");
+						countCache = Integer.parseInt(asieCount());
+						hasSet = true;
+					}
+				} else {
+					hasSet = false;
 				}
 			}
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
+
 	}
 
-	@Override
-	public EnumSet<TickType> ticks() {
-		return EnumSet.of(TickType.CLIENT, TickType.RENDER);
-	}
-
-	@Override
-	public String getLabel() {
-		return "Drama";
+	public void tickEnd() {
+		try {
+			Minecraft mc = Minecraft.getMinecraft();
+			if (mc.currentScreen != null) {
+				if (mc.currentScreen instanceof GuiMainMenu) {
+					GuiMainMenu mm = (GuiMainMenu) mc.currentScreen;
+					mm.drawString(mc.fontRenderer, countCache
+							+ " dramas generated and counting!", 2, 2, 0xffffff);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
